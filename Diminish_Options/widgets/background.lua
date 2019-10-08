47c2b1894a1baa207c4f6d090a5ca0a7c0ba021b
wardz <thomaszrx@gmail.com>
2019-10-08 14:53:44 +0200

    add widget helper for creating bg

---
+++ Diminish_Options/widgets/background.lua
@@ -0,0 +1,17 @@
+local _, NS = ...
+local Widgets = NS.Widgets
+
+local panelBackdrop = {
+	bgFile = [[Interface\Tooltips\UI-Tooltip-Background]], tile = true, tileSize = 16,
+	edgeFile = [[Interface\Tooltips\UI-Tooltip-Border]], edgeSize = 16,
+	insets = { left = 5, right = 5, top = 5, bottom = 5 }
+}
+
+function Widgets:CreatePanelBackground(parent)
+	local frame = CreateFrame("Frame", nil, parent)
+	frame:SetBackdrop(panelBackdrop)
+	frame:SetBackdropColor(0.06, 0.06, 0.06, 0.4)
+	frame:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
+
+    return frame
+end
