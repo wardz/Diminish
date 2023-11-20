07c2404d95ae8ca109bd457343f775d807166706
wardz <10900212+wardz@users.noreply.github.com>
2023-11-20 13:25:02 +0100

    remove non-english localizations as bigwigs packager and/or curseforge API seems bugged atm

--- Diminish_Options/locales/localization.lua
+++ Diminish_Options/locales/localization.lua
@@ -111,24 +111,3 @@ L["ZONE_OUTDOORS"] = "World"
 L["ZONE_SCENARIO"] = "Scenario"
 L["ZONE_RAIDS"] = "Raids"
 L["ZONES_TOOLTIP"] = "Enable tracking for this zone."
-
--- luacheck: push ignore 542
-local locale = GetLocale()
-if locale == "deDE" then
-    --@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "frFR" then
-    --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "itIT" then
-    --@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "koKR" then
-    --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "ptBR" then
-    --@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "ruRU" then
-    --@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "esES" or locale == "esMX" then
-    --@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore")@
-elseif locale == "zhCN" or locale == "zhTW" then
-    --@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
-end
--- luacheck: pop
