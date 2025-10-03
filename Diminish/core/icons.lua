bd248b22311e0e6c0ab575c18266061d51ee57a4
wardz <10900212+wardz@users.noreply.github.com>
2025-10-03 12:36:46 +0200

    remove obsolete 'announceDRs' code

--- Diminish/core/icons.lua
+++ Diminish/core/icons.lua
@@ -266,14 +266,7 @@ do
         local frame = self.parent
         local timer = frame.timerRef
 
-        if timer and self:GetCooldownDuration() <= 0.2 then
-            if frame.unitFormatted and C_VoiceChat and C_VoiceChat.SpeakText then
-                if DIMINISH_NS.db.announceDRs and not timer.isNotPetOrPlayer then -- FYI: experimental/test at the moment
-                    local text = format("%s %s expired.", UnitName(frame.unitFormatted) or "", timer.category)
-                    C_VoiceChat.SpeakText(0, text, Enum.VoiceTtsDestination.QueuedLocalPlayback, 3, 100)
-                end
-            end
-
+        if timer and self:GetCooldownDuration() < 0.2 then
             NS.Timers:Remove(timer.unitGUID, timer.category)
         end
 
